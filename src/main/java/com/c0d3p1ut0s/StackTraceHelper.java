ea688a35ff7af2bfb5a2a6fcaa002a3291681b07
c0d3p1ut0s <c0d3p1ut0s@gmail.com>
2018-04-12 11:06:37 +0800

    Optimize: output format

--- src/main/java/com/c0d3p1ut0s/StackTraceHelper.java
+++ src/main/java/com/c0d3p1ut0s/StackTraceHelper.java
@@ -12,8 +12,7 @@ public class StackTraceHelper {
     public static void enterMethod(StackTraceElement[] stackTraceElements){
         System.out.println("JavaStackTrace: ");
         for(StackTraceElement stackTraceElement:stackTraceElements){
-            System.out.println("\tat "+stackTraceElement.getClassName()+"."+stackTraceElement.getMethodName()+"("
-                    +stackTraceElement.getFileName()+":"+stackTraceElement.getLineNumber()+")");
+            System.out.println("\tat "+stackTraceElement);
         }
         System.out.println("");
     }
